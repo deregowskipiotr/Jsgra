fe7476e91fdc6f72db781af222f64dac82c9659c
Piotr Deregowski <deregowskipiotr@interia.pl>
2022-06-21 12:03:43 +0200

    Add new script.js file

--- js/script.js
+++ js/script.js
@@ -6,3 +6,30 @@ printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wy
 var playerMove;
 playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+
+var randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+printMessage('Wylosowana liczba to: ' + randomNumber);
+
+
+
+
+
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} 
+else if (randomNumber=='2') {
+    computerMove = 'papier'
+}
+else if (randomNumber == '3') {
+    computerMove = 'nożyce'
+}
+
+else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
