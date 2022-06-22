b42ac135e084034a9c7af7048fe99f288cc27803
Piotr Deregowski <deregowskipiotr@interia.pl>
2022-06-22 14:32:42 +0200

    Add css style btn/game

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 
 const game = ( ) => {
-    let buttons = document.querySelectorAll('btn');
+    let allChoose = document.querySelectorAll('.btn');
     let winner = document.getElementById('winner');
     let resultUser = document.getElementById('result-user');
     let resultComputer = document.getElementById('result-computer');
     let sectionResult = document.getElementById('result');
+    let scoreUserDisplay = document.getElementById('scoreUser');
+    let scoreComputerDisplay = document.getElementById('scoreComputer');
+    let scoreUser = 0;
+    let scoreComputer = 0;
     let computerChoosed = '';
     let userChoosed = '';
     let time = 3;
 
     const startGame = () => {
-        buttons.forEach(user => {
+        allChoose.forEach(user => {
             user.addEventListener('click', () => {
                 prepareToStartGame(user);
                 let timeInterval = setInterval(() => {
@@ -18,12 +22,13 @@ const game = ( ) => {
                         clearInterval(timeInterval);
                         let computer = Math.floor(Math.random() *3);
                         displayUserAndComputer(buttons.id, allChoose[computer].id );
+                        checkWin(user.id, allChoose[computer].id);
                         sectionResult.classList.add('result');
                     } else {
                         winner.textContent = time;
                         time --;
                     }
-                }, 800)
+                }, 200)
             })
         })
     }
@@ -39,7 +44,7 @@ const game = ( ) => {
 
 
     prepareToStartGame = (user) => {
-        buttons.forEach(val => val.classList.remove('green'));
+        allChoose.forEach(val => val.classList.remove('green'));
         time = 3;
         winner.textContent = time;
 
@@ -47,6 +52,52 @@ const game = ( ) => {
         sectionResult.classList.remove('result');
     }
 
+    checkWin = (user,computer) => {
+        if (user === computer) {
+            winner.textContent = "REMIS";
+            return;
+        }
+        if (user === "rock") {
+            if (computer === "scissors"){
+            winner.textContent = "WYGRAŁAEŚ";
+            scoreUser++;
+            scoreUserDisplay.textContent = scoreUser;
+            return;
+        } else {
+            winner.textContent = "PRZEGRAŁEŚ";
+            scoreComputer++;
+            scoreComputerDisplay.textContent = scoreComputer;
+            return;
+        }
+        }
+        if (user === "paper") {
+            if(computer === "scissors") {
+                winner.textContent = "PRZEGRAŁEŚ";
+                scoreComputer++;
+                scoreComputerDisplay.textContent = scoreComputer;
+                return;
+            } else {
+                winner.textContent = "WYGRAŁAEŚ";
+                scoreUser++;
+                scoreUserDisplay.textContent = scoreUser;
+                return;
+            }
+        }
+        if (user === "scissors") {
+            if(computer === "rock") {
+                winner.textContent = "PRZEGRAŁEŚ";
+                scoreComputer++;
+                scoreComputerDisplay.textContent = scoreComputer;
+                return;
+            } else {
+                winner.textContent = "WYGRAŁEŚ";
+                scoreUser++;
+                scoreUserDisplay.textContent = scoreUser;
+                return;
+            }
+        }
+      
+    }
     startGame()
-}
-game()
+    }
+    game()  
